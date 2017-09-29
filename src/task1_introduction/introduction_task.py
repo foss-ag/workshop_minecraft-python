b7413e8899e683f0847d89bc06d3fcd4cbb1e1ef
Alexander Becker <alexander2.becker@udo.edu>
2017-09-29 13:01:19 +0200

    prepared task and solution files

--- src/task1_introduction/introduction_task.py
+++ src/task1_introduction/introduction_task.py
@@ -5,9 +5,4 @@ mc = minecraft.Minecraft.create()
 # flower block ID
 flower = 38
 
-# this loop runs forever
-while True:
-    # get players current position
-    (x, y, z) = mc.player.getPos()
-    # place flowers at players position
-    mc.setBlock(x, y, z, flower)
+# your code here
