b8ad22a1bbf8d5e850deba793a3e00960cf6a937
overflow <alexander2.becker@udo.edu>
2018-11-10 23:47:04 +0100

    [add] documentation

--- src/astroids/GameState.py
+++ src/astroids/GameState.py
@@ -1,6 +1,22 @@
-class GameState():
+class GameState:
 
     def __init__(self, game_minutes=4, lifes=5, player_speed=3, astroid_speed=7, astroid_timer=100, astroids_faster=0):
+        """
+        Create game state object.
+
+        :param game_minutes:
+            Game duration in minutes (default: 4)
+        :param lifes:
+            Player lifes (default: 5)
+        :param player_speed:
+            Player speed (default: 3)
+        :param astroid_speed:
+            Astroid speed (default: 7)
+        :param astroid_timer:
+            Initial astroid timer. Frequency of spawning astroids in milliseconds (default: 100)
+        :param astroids_faster:
+            Milliseconds reducing the astroid timer (default: 0)
+        """
         self.__done = False
         self.__game_minutes = game_minutes
         self.__lifes = lifes
@@ -10,9 +26,11 @@ class GameState():
         self.__astroid_timer = astroid_timer
         self.__astroids_faster = astroids_faster
 
+        # list of current astroids and shots
         self.__astroids = []
         self.__shots = []
 
+        # number of hits and shots
         self.__num_hits = 0
         self.__num_shots = 0
 
@@ -61,31 +79,79 @@ class GameState():
         return self.__num_shots
 
     def set_done(self):
+        """
+        Game is done!
+        """
         self.__done = True
 
     def add_astroid(self, astroid):
+        """
+        Add astroid to astroid list
+
+        :param astroid:
+            Astroid object
+        """
         self.__astroids.append(astroid)
 
     def remove_astroid(self, astroid):
+        """
+        Remove astroid from astroid list
+
+        :param astroid:
+            Astroid object
+        """
         self.__astroids.remove(astroid)
 
     def remove_shot(self, bullet):
+        """
+        Remove shot from shot list
+
+        :param bullet:
+            Bullet object
+        """
         self.__shots.remove(bullet)
 
     def add_shot(self, bullet):
+        """
+        Add shot to shot list
+
+        :param bullet:
+            Bullet object
+        """
         self.__shots.append(bullet)
 
     def set_astroid_timer(self, t):
+        """
+        Set astroid timer.
+
+        :param t:
+            New astroid timer
+        """
         self.__astroid_timer = t
 
     def set_astroids_faster(self, s):
+        """
+        Set astroids faster.
+
+        :param s:
+            New astroid timer reduction.
+        """
         self.__astroids_faster = s
 
     def reduce_lifes(self):
+        """
+        Reduce player life by 1.
+        """
         self.__lifes -= 1
 
     def increment_num_shots(self):
+        """
+        Increment number of shots by 1.
+        """
         self.__num_shots += 1
 
     def increment_num_hits(self):
+        """
+        Increment number of hits by 1.
+        """
         self.__num_hits += 1
