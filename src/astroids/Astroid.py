c5a120591b825c5bccd61c9fd0a32811c236ef3f
overflow <alexander2.becker@udo.edu>
2018-11-10 23:34:22 +0100

    [add] documentation

--- src/astroids/Astroid.py
+++ src/astroids/Astroid.py
@@ -2,22 +2,44 @@ import pygame
 import random
 
 
-class Astroid():
+class Astroid:
 
     def __init__(self, x, y, scale):
+        """
+        Create Astroid object.
+
+        :param x:
+            Start position X.
+        :param y:
+            Start position Y.
+        :param scale:
+            Astroid size scale.
+        """
+        # astroid position
         self.__x = x
         self.__y = y
+        # astroid size scale
         self.__scale = scale
+        # hit count
         self.__hit_count = 0
+        # astroid image
         self.__img = pygame.image.load('src/astroid.png')
 
+        # scale astroid image and get astroid rectangle
         size = self.__img.get_size()
         self.__img = pygame.transform.scale(self.__img, (size[0] * self.__scale, size[1] * self.__scale))
         self.__astroid_rect = pygame.Rect(self.__img.get_rect())
 
     @staticmethod
     def create_astroid(state, size):
-        # x pos, y pos, size scale, shooted
+        """
+        Create new random astroid and add it to the astroids list in the game state.
+
+        :param state:
+            Game state with astroids list.
+        :param size:
+            Screen size.
+        """
         state.add_astroid(Astroid(size[0] - 5, random.randint(50, size[1] - 30), random.randint(1, 6)))
 
     @property
@@ -45,20 +67,30 @@ class Astroid():
         return self.__img
 
     def get_rect(self):
+        """
+        Get new astroid rectangle for new position.
+
+        :return:
+            Astroid rectangle
+        """
         self.__astroid_rect.top = self.__y
         self.__astroid_rect.left = self.__x
         return self.__astroid_rect
 
+
+
     def increment_hit_count(self):
+        """
+        Increment astroid hit count by 1.
+        """
         self.__hit_count += 1
 
     def move(self, x):
         """
-        TODO
+        Update astroid position by x offset.
 
         :param x:
             x-offset
-        :return:
         """
         self.__x += x
 
