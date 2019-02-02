7d468b9c88c2b11564935bb161c1fb12b8584772
overflow <alexander2.becker@udo.edu>
2019-02-02 16:55:24 +0100

    moved global variables to Player class

--- src/vvvvvv/Player.py
+++ src/vvvvvv/Player.py
@@ -17,6 +17,30 @@ class Player:
         # player position
         self.__x = x
         self.__y = y
+        # player still alive?
+        self.__dead = False
+        # movement variables
+        self.__move_up = True
+        self.__move_right = False
+        self.__move_left = False
+        # movement speed
+        self.__speed = 5
+
+    @property
+    def dead(self):
+        return self.__dead
+
+    @property
+    def move_up(self):
+        return self.__move_up
+
+    @property
+    def move_left(self):
+        return self.__move_left
+
+    @property
+    def move_right(self):
+        return self.__move_right
 
     @property
     def pos(self):
@@ -26,13 +50,37 @@ class Player:
     def image(self):
         return self.__img
 
-    def move(self, x, y):
+    def change_move_up(self):
         """
-        Change player position by given offset
-        :param x:
-            x-axis offset
-        :param y:
-            y-axis offset
+        Changes move_up to False if True and vice versa.
         """
-        self.__x += x
-        self.__y += y
+        self.__move_up = not self.__move_up
+
+    def set_move_left(self, b):
+        """
+        Changes move_left given Boolean b.
+
+        :param b: Boolean.
+        """
+        self.__move_left = b
+
+    def set_move_right(self, b):
+        """
+        Changes move_right to given Boolean b.
+
+        :param b: Boolean
+        """
+        self.__move_right = b
+
+    def move(self):
+        """
+        Update player position depending on move variables.
+        """
+        self.__y += self.__speed * self.__move_up - self.__speed * (not self.__move_up)
+        self.__x += self.__speed * self.__move_right - self.__speed * self.__move_left
+
+    def kill(self):
+        """
+        Change dead status variable to True.
+        """
+        self.__dead = True
--- src/vvvvvv/vvvvvv.py
+++ src/vvvvvv/vvvvvv.py
@@ -9,46 +9,32 @@ screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
 player = Player(500, 400)
-speed = 5
 
-dead = False
-move_up = True
-move_right = False
-move_left = False
-
-while not dead:
+while not player.dead:
     screen.fill((0, 0, 0))
 
     # check pressed buttons
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                dead = True
+                player.kill()
                 continue
             if event.key == pygame.K_SPACE:
-                move_up = not move_up
+                player.change_move_up()
             if event.key == pygame.K_LEFT:
-                move_left = True
-                move_right = False
+                player.set_move_left(True)
+                player.set_move_right(False)
             if event.key == pygame.K_RIGHT:
-                move_right = True
-                move_left = False
+                player.set_move_left(False)
+                player.set_move_right(True)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
-                move_left = False
+                player.set_move_left(False)
             if event.key == pygame.K_RIGHT:
-                move_right = False
+                player.set_move_right(False)
 
-    if move_up:
-        player.move(0, speed)
-    else:
-        player.move(0, -speed)
-
-    if move_right:
-        player.move(speed, 0)
-    if move_left:
-        player.move(-speed, 0)
+    player.move()
 
     screen.blit(player.image, player.pos)
 
