1eefd857a43205800fac95b0e5c460ec5031809d
overflow <alexander2.becker@udo.edu>
2019-02-05 21:49:29 +0100

    added player score

--- src/vvvvvv/vvvvvv.py
+++ src/vvvvvv/vvvvvv.py
@@ -7,8 +7,12 @@ size = (1000, 800)
 screen = pygame.display.set_mode(size)
 
 clock = pygame.time.Clock()
-
 player = Player(500, 400)
+
+# score variables
+multiplier = 1
+bonus = 20
+score = 0
 
 # define border height, width and thickness
 border_height = 35
@@ -18,8 +22,17 @@ border_width = 1000
 white = (255, 255, 255)
 grey = (150, 150, 150)
 
+def write_score():
+    font = pygame.font.Font(None, 24)
+    score_text = font.render(str(score), True, (255, 255, 255))
+    score_text_rect = score_text.get_rect()
+    score_text_rect.topleft = [10, 10]
+    screen.blit(score_text, score_text_rect)
+
+
 while not player.dead:
     screen.fill((0, 0, 0))
+    write_score()
 
     # draw upper and lower bounds
 
@@ -29,10 +42,12 @@ while not player.dead:
         pygame.draw.rect(screen, grey, (0, border_height, border_width, 3), 3)
         pygame.draw.rect(screen, white, (0, size[1] - border_height, border_width, 3), 3)
         player.flip()
+        score += bonus * multiplier
     elif y >= 710:
         pygame.draw.rect(screen, white, (0, border_height, border_width, 3), 3)
         pygame.draw.rect(screen, grey, (0, size[1] - border_height, border_width, 3), 3)
         player.flip()
+        score += bonus * multiplier
     else:
         pygame.draw.rect(screen, white, (0, border_height, border_width, 3), 3)
         pygame.draw.rect(screen, white, (0, size[1] - border_height, border_width, 3), 3)
